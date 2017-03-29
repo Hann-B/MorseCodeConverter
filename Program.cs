b38f1ebb34dcf5ede3b6e75d71d6bc27d429d306
hanna <hlbernard124@gmail.com>
2017-03-29 18:42:25 -0400

    level 1 completed!

--- Program.cs
+++ Program.cs
@@ -9,36 +9,71 @@ namespace MorseCodeConverter
 {
     class Program
     {
+        static string codePath = "morse.csv.txt";
+        static string answerPath = "Translations.txt";
 
-
-        static void Main(string[] args)
+        public static Dictionary<string, string> CompileMorseCode()
         {
-            string codePath = "morse.csv.txt";
             var morseCode = new Dictionary<string, string>();
-
             using (var reader = new StreamReader(codePath))
             {
                 while (reader.Peek() > -1)
                 {
                     var message = reader.ReadLine();
                     var split = message.Split(',');
-                    morseCode.Add(split[0],split[1]);
+                    morseCode.Add(split[0], split[1]);
                 }
             }
             morseCode.Add(" ", " ");
+            return morseCode;
+        }
+        public static string AskForMessage()
+        {
             Console.WriteLine("Enter a message:");
             var sentence = Console.ReadLine().ToUpper();
+            return sentence;
+        }
+        public static void DisplayTranslation(string sentence, Dictionary<string, string>morseCode)
+        {
             foreach (var letter in sentence)
             {
                     Console.Write(morseCode[letter.ToString()]);
             }
-            string answerPath = "Translations.txt";
+        }
+        public static void WriteToCSV(string sentence, Dictionary<string, string>morseCode)
+        {
             using (StreamWriter w = File.AppendText(answerPath))
             {
-                
+                foreach (var letter in sentence)
+                {
+                    w.Write(morseCode[letter.ToString()]);
+                }
+                w.Write($"{sentence}");
             }
-            Console.WriteLine("Another message?:");
-            var answer = Console.ReadLine();
+        }
+        public static string AnotherMessage()
+        {
+            Console.WriteLine("Another message?(Y)es/(N)o");
+            string answer = Console.ReadLine().ToUpper();
+            return answer;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Dictionary <string,string>morseCode = CompileMorseCode();
+            var sentence = AskForMessage();
+            DisplayTranslation(sentence,morseCode);
+            WriteToCSV(sentence,morseCode);
+            string answer = AnotherMessage();
+            if (answer == "Y")
+            {
+                AskForMessage();
+                DisplayTranslation(sentence, morseCode);
+                WriteToCSV(sentence,morseCode);
+                AnotherMessage();
+            }
+            else { Console.WriteLine("Thanks for visiting!"); }               
         }
     }
 }
